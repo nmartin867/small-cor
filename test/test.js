32c1ce08f1417665539f2bdb48b51165f70f8d42
nmartin <nmartin867@gmail.com>
2013-07-28 23:09:03 -0600

    Get mocha and supertest tests setup

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var request = require('supertest')
+  , express = require('express');
+
+var app = express();
+
+  describe('Headers', function(){
+  /*it('contains', function(done){
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })*/
+})
