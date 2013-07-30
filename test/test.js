d918f8c891cb4a01af7230768a6ecc80106db145
Nick Martin <nmartin867@gmail.com>
2013-07-29 19:54:26 -0600

    Adding mocha tests for single header values

--- test/test.js
+++ test/test.js
@@ -1,14 +1,38 @@
 var request = require('supertest')
-  , express = require('express');
+, express = require('express')
+, smallCOR = require('../index.js');
 
 var app = express();
 
-  describe('Headers', function(){
-  /*it('contains', function(done){
-    request(app)
-      .get('/user')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-  })*/
+app.use(smallCOR({
+			origins : ['*'],
+			methods : ['GET'],
+			headers : ['X-Custom']
+		}));
+
+app.get('/', function(req, res){  
+	res.send('hello world');
+});
+
+describe('Headers', function(){
+	it('contains a single custom header', function(done){		
+		request(app)
+		.get('/')      
+		.expect('Access-Control-Allow-Headers', 'X-Custom')    
+		.expect(200, done);
+	})
+
+	it('contains GET as the only Access Control Allow Method', function(done){		
+		request(app)
+		.get('/')      
+		.expect('Access-Control-Allow-Methods', 'GET')    
+		.expect(200, done);
+	})
+
+	it('contains * as the only Access Control Allow Origin', function(done){		
+		request(app)
+		.get('/')      
+		.expect('Access-Control-Allow-Origin', '*')    
+		.expect(200, done);
+	})
 })
